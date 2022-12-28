55681c63400f780ad6eb7f83797cd02f97b9f531
MaverickMX <Jonathanmoreno996@gmail.com>
2022-12-28 01:01:05 -0600

    añadi el header

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+   <nav className='menu'>
+        <ul className='menu-list'>
+            <li className='menu-item'><button>Playeras</button></li>
+            <li className='menu-item'><button>Hoodies</button></li>
+            <li className='menu-item'><button>Tote Bags</button></li>
+        </ul>
+   </nav>
+  )
+}
+
+export default NavBar
